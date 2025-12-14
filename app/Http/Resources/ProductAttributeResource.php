14b3672869dcc6aea864ef041cc4a3adf61da893
JM Mac <johnmarkmancol@gmail.com>
2025-12-14 20:11:14 +0800

    fix: missing id in product attribute response

--- app/Http/Resources/ProductAttributeResource.php
+++ app/Http/Resources/ProductAttributeResource.php
@@ -15,6 +15,7 @@ class ProductAttributeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'product_id' => $this->product_id,
             'attribute_id' => $this->attribute_id,
             'attribute' => $this->whenLoaded('attribute'),
