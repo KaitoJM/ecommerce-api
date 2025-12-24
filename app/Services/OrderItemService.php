c4c8348fc4e12d930a9d16d03b3cd7262fed7d06
JM Mac <johnmarkmancol@gmail.com>
2025-12-24 17:44:18 +0800

    chore: order and order item service

---
+++ app/Services/OrderItemService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\OrderItem;
+use Carbon\Carbon;
+
+class OrderItemService {
+    public function getOrderItems(?string $search = null, $filters = null, $pagination = null) {
+        return OrderItem::when($search, function($query) use ($search) {
+            $query->where(function ($q) use ($search) {
+                $q->where('product_snapshot_name', 'like', "%{$search}%");
+                $q->orWhere('product_snapshot_price', 'like', "%{$search}%");
+            });
+        })->when(isset($filters['product_id']), function($query) use ($filters) {
+            $query->where('product_id', $filters['product_id']);
+        })->when(isset($filters['order_id']), function($query) use ($filters) {
+            $query->where('order_id', $filters['order_id']);
+        })->when(isset($filters['product_specification_id']), function($query) use ($filters) {
+            $query->where('product_specification_id', $filters['product_specification_id']);
+        })->paginate($pagination['per_page'] ?? 10);
+    }
+
+    /**
+     *
+     * @param  array{
+     *     order_id: string,
+     *     product_id: string,
+     *     product_specification_id: string,
+     *     product_snapshot_name: string,
+     *     product_snapshot_price: float,
+     *     quantity: integer,
+     *     total: float,
+     * }  $params
+     * @return \App\Models\OrderItem
+     */
+    public function createOrderItem($params) {
+        $createdOrderItem = OrderItem::create([
+            'order_id' => $params['order_id'],
+            'product_id' => $params['product_id'],
+            'product_specification_id' => $params['product_specification_id'],
+            'product_snapshot_name' => $params['product_snapshot_name'],
+            'product_snapshot_price' => $params['product_snapshot_price'],
+            'quantity' => $params['quantity'],
+            'total' => $params['total'] ?? 0,
+        ]);
+
+        return $createdOrderItem;
+    }
+
+    /**
+     * Get an order item by its ID.
+     *
+     * @param int $id The ID of the order item to get
+     * @return \App\Models\OrderItem
+     */
+    public function getOrderItemById(int $id) {
+        return OrderItem::findOrFail($id);
+    }
+
+    /**
+     * Update an order item by its ID.
+     *
+     * @param int $id The ID of the order item to update
+     * @param array $params The parameters to update the order item with
+     * @return \App\Models\OrderItem
+     */
+    public function updateOrderItem(int $id, array $params) {
+        $orderItem = $this->getOrderItemById($id);
+
+        $orderItem->update($params);
+
+        return $orderItem;
+    }
+
+    /**
+     * Delete an order item by its ID.
+     *
+     * @param int $id The ID of the order item to delete
+     * @return \App\Models\OrderItem
+     */
+    public function deleteOrderItem(int $id) {
+        $orderItem = $this->getOrderItemById($id);
+
+        $orderItem->delete();
+
+        return $orderItem;
+    }
+}
---
+++ app/Services/OrderService.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Order;
+use Carbon\Carbon;
+
+class OrderService {
+    public function getOrder(?string $search = null, $filters = null, $pagination = null) {
+        return Order::when(isset($filters['user_id']), function($query) use ($filters) {
+            $query->where('user_id', $filters['user_id']);
+        })->when(isset($filters['session_id']), function($query) use ($filters) {
+            $query->where('session_id', $filters['session_id']);
+        })->when(isset($filters['cart_id']), function($query) use ($filters) {
+            $query->where('cart_id', $filters['cart_id']);
+        })->when(isset($filters['status_id']), function($query) use ($filters) {
+            $query->where('status_id', $filters['status_id']);
+        })->paginate($pagination['per_page'] ?? 10);
+    }
+
+    /**
+     *
+     * @param  array{
+     *     user_id: string,
+     *     seesion_id: string,
+     *     cart_id: string,
+     *     status_id: string,
+     *     subtotal: float,
+     *     discount_total: float,
+     *     tax_total: float,
+     *     total: float,
+     * }  $params
+     * @return \App\Models\Order
+     */
+    public function createOrder($params) {
+        $createdOrder = Order::create([
+            'user_id' => $params['user_id'] ?? null,
+            'session_id' => $params['session_id'] ?? null,
+            'cart_id' => $params['cart_id'],
+            'status_id' => $params['status_id'],
+            'subtotal' => $params['subtotal'] ?? 0,
+            'discount_total' => $params['discount_total'] ?? 0,
+            'tax_total' => $params['tax_total'] ?? 0,
+            'total' => $params['total'] ?? 0,
+        ]);
+
+        return $createdOrder;
+    }
+
+    /**
+     * Get a order by its ID.
+     *
+     * @param int $id The ID of the order to get
+     * @return \App\Models\Order
+     */
+    public function getOrderById(int $id) {
+        return Order::findOrFail($id);
+    }
+
+    /**
+     * Update an order by its ID.
+     *
+     * @param int $id The ID of the order to update
+     * @param array $params The parameters to update the order with
+     * @return \App\Models\Order
+     */
+    public function updateOrder(int $id, array $params) {
+        $order = $this->getOrderById($id);
+
+        $order->update($params);
+
+        return $order;
+    }
+
+    /**
+     * Delete an order by its ID.
+     *
+     * @param int $id The ID of the order to delete
+     * @return \App\Models\Order
+     */
+    public function deleteOrder(int $id) {
+        $order = $this->getOrderById($id);
+
+        $order->delete();
+
+        return $order;
+    }
+}
