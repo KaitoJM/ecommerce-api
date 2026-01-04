6add5a77ee7e8aa7508cd517405e4ffbc175cd40
JM Mac <johnmarkmancol@gmail.com>
2026-01-05 01:08:57 +0800

    docs: wrote code documentation for auth service methods and variables

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -9,7 +9,15 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 
 class AuthService {
+    /**
+     * @var \App\Models\User
+     * Holds the authenticated user data
+     */
     public $user;
+    /**
+     * @var string
+     * The generated personal access token of the authenticated user
+     */
     public $token;
 
     public function __construct(
@@ -17,6 +25,17 @@ class AuthService {
         protected CustomerRepository $customerRepository
     ){}
 
+    /**
+     * Authenticates a user and store the authentication data on public variables
+     *
+     * @param  array{
+     *     email: string
+     *     role?: string
+     * } $params
+     * @param string $password
+     * @param string $tokenKey
+     *
+     */
     public function authenticate($params, string $password, string $tokenKey = "backoffice-app") {
         $user = $this->userRepository->getUserSingle($params);
 
@@ -34,7 +53,16 @@ class AuthService {
         $this->token = $token->plainTextToken;
     }
 
+    /**
+     * Retrieves the customer data using the stored authenticated user data
+     *
+     * @return \App\Models\Customer
+     */
     public function getCustomer() {
+        if (!$this->user) {
+            throw new Exception("No athenticated user.");
+        }
+
         $customer = $this->customerRepository->getCustomerSingle(['user_id' => $this->user->id]);
         return $customer;
     }
